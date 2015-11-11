fc8712eaee661f6da7efb3cd124d8fb8f574e099
ikaros <ikaros@ikaros-desktop.(none)>
2015-11-11 22:05:21 +0900

    add gaussian

--- src/activity_rec.cpp
+++ src/activity_rec.cpp
@@ -4,9 +4,10 @@
 #include <vector>
 #include <fstream>
 #include "hCRF.h"
-#include <std_msgs/String.h>
+//#include <std_msgs/String.h>
 #include <sstream>
 #include "GraphUtils.h"
+#include <human_action_recognition/activityRecognition.h>
 using namespace std;
 
 
@@ -26,6 +27,7 @@ public:
   vector<float> result;
   vector<vector<float> > visual_result;
   vector<string> label_name;
+  float gaussian_mask[5];
   int nblabel;
   int counter;
   dMatrix* sk_data;
@@ -33,10 +35,13 @@ public:
   Toolbox* toolbox;
   Model* pModel;
   InferenceEngine* pInfEngine;
-
+  
 
   activity_recognition(int frame_len,int sk_num)
-    :frame_length(frame_len),sk_feature_num(sk_num),counter(0)
+    :frame_length(frame_len),
+     sk_feature_num(sk_num),
+     counter(0),
+     gaussian_mask{0.054488685,0.24420135,0.40261996,0.24420135,0.054488685}
   {
     current_frame = 0;    
     sk_data = new dMatrix(frame_length,sk_feature_num,0);
@@ -52,10 +57,10 @@ public:
 	  vector<float> vec;
 	  visual_result.push_back(vec);
 	}
-    string label[3]={"working","shake hand","drinking"};
-    label_name.assign(label, label+3);
+    string label[4]={"working","shake hand","drinking","11"};
+    label_name.assign(label, label+4);
 
-    action_result_pub=nh_.advertise<std_msgs::String>("query_result",1);
+    action_result_pub=nh_.advertise<human_action_recognition::activityRecognition>("query_result",1);
  }
 
   ~activity_recognition()
@@ -91,7 +96,22 @@ public:
     return v;
   }  
 
-
+  float gaussian_smooth(vector<float> v) //since its a sliding, we only need to smooth the third to last element;  
+  {
+    float value;
+    vector<float>::reverse_iterator iter=v.rbegin(); 
+    if(v.size() < 5) 
+      return 0;
+    else
+      {
+	for(int i=0; i<5; i++)
+	  {
+	    value += *iter*gaussian_mask[i];
+	    iter++;
+	  }
+	return value;
+      }
+  }
 
 
 };
@@ -107,61 +127,33 @@ int main(int argc, char** argv)
   ros::Rate rate(10.0);
   tf::TransformListener listener;
   tf::StampedTransform transform;
-  vector<tf::StampedTransform> transform_v;      
+  vector<tf::StampedTransform> transform_v; 
+ 
+  string frame_name[9]={"/head_1","/neck_1","/torso_1","/left_shoulder_1","/left_elbow_1","/left_hand_1","/right_shoulder_1","/right_elbow_1","/right_hand_1"};
 
-
-  
   while(ar.nh_.ok())
     {
-
       try{
+	for(int i=0 ; i<9; i++){
+	 
+	  listener.lookupTransform("/openni_depth_frame", frame_name[i] ,ros::Time(0) ,transform);
 
-      listener.lookupTransform("/openni_depth_frame", "/head_1",ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame", "/neck_1",ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/torso_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-
-      listener.lookupTransform("/openni_depth_frame","/left_shoulder_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame", "/left_elbow_1",ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/left_hand_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-
-      listener.lookupTransform("/openni_depth_frame","/right_shoulder_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/right_elbow_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/right_hand_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      
-      /*      listener.lookupTransform("/openni_depth_frame","/left_hip_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/left_knee_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/left_foot_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-
-      listener.lookupTransform("/openni_depth_frame","/right_hip_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/right_knee_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);
-      listener.lookupTransform("/openni_depth_frame","/right_foot_1", ros::Time(0) ,transform);
-      transform_v.push_back(transform);*/
+	  transform_v.push_back(transform);
+	}	
       }
       catch (tf::TransformException ex)
         {
+	  
           ros::Duration(1.0).sleep();
         }
-      if(!transform_v.empty())
+
+       if(!transform_v.empty())
 	{
 	
 	  vector<double> d_l = ar.v_sub(transform_v[5],transform_v[4]);
 	  vector<double> d_r = ar.v_sub(transform_v[8],transform_v[7]);      
 	  double ref_dis=max(ar.v_norm(d_l), ar.v_norm(d_r)); //use the ||left/right hand - elbow|| as reference distance
-	  //        cout<<"distance"<<ref_dis<<endl;
+	  // cout<<"distance"<<ref_dis<<endl;
 	  vector<vector<double> > p(transform_v.size()-1);
 	
 	  for(int i=0; i<transform_v.size()-1; i++)
@@ -188,20 +180,22 @@ int main(int argc, char** argv)
 	    ar.sk_data->slid_window(frame_length,sk_feature_num);
 	    
 	  ar.data_seq->setPrecomputedFeatures(ar.sk_data);
-	  
-	  
-	  //cout<<"result "<<ar.toolbox->realtimeLabelOutput(ar.data_seq)<<endl;
+
+
+	  // cout<<"result "<<ar.toolbox->realtimeLabelOutput(ar.data_seq)<<endl;
 	  //int label = ar.toolbox->realtimeLabelOutput(ar.data_seq);
 	  stringstream ss;
-	  std_msgs::String action_result;
+	  human_action_recognition::activityRecognition action_result;
 	  //ar.result[label]++;
 	 
 	  dMatrix* score;
 	  int score_sum=0;
 	  score = ar.toolbox->realtimeScoreOutput(ar.data_seq);
+	 
 	  for(int i=0; i<ar.nblabel; i++)
 	    {
 	      ar.result[i] += score->getValue(i,0);	      
+	       
 	    }
 	  
 	  //----------------------------------
@@ -219,70 +213,35 @@ int main(int argc, char** argv)
 		  for(int i=0; i<ar.nblabel; i++)
 		    {
 		      ar.result[i] = ar.result[i]/score_sum;
-		      if(ar.visual_result[i].size()>=frame_length)
-			ar.visual_result[i].erase(ar.visual_result[i].begin());
-		      ar.visual_result[i].push_back(ar.result[i]);
 		    
+		      if(ar.visual_result[i].size()>=frame_length)
+			{
+			  ar.visual_result[i].erase(ar.visual_result[i].begin());
+			  ar.visual_result[i][frame_length-3]=ar.gaussian_smooth(ar.visual_result[i]);
+			  if(ar.gaussian_smooth(ar.visual_result[i]) > 0.8)
+			    {
+			      cout<<"going to pub"<<endl;
+			    }
+			}
+			  
+		      ar.visual_result[i].push_back(ar.result[i]);
+		      		
 		    }
 		  
 		  IplImage* graph=drawFloatGraph(&ar.visual_result[0][0], ar.visual_result[0].size(),NULL, 0, 1, 400, 200, ar.label_name[0].c_str());
 		  if(ar.visual_result[0].size()>=frame_length)
 		    {
 		      for(int i=1; i<ar.nblabel; i++)
-			drawFloatGraph(&ar.visual_result[i][0], ar.visual_result[i].size() ,graph, 0, 1, 400, 200, ar.label_name[i].c_str());
+			drawFloatGraph(&ar.visual_result[i][0], ar.visual_result[i].size()-2 ,graph, 0, 1, 400, 200, ar.label_name[i].c_str());//output the graph have been smoothed
 		      showImage(graph,10);
 		      setGraphColor(0);
 		    }
-		      
-		  listener.clear();
-		  //----------------------------
-	  /*if(ar.counter>=3)
-		{
-		  int lowest_score = ar.result[0];
-		  for(int i=1; i<ar.nblabel; i++)
-		    {
-		      if(ar.result[i] <= lowest_score)
-			lowest_score = ar.result[i];		      
-		    }
-		  for(int i=0; i<ar.nblabel; i++)
-		    {
-		      ar.result[i] -= lowest_score;
-		      score_sum += ar.result[i]; 
-		    }
-		  for(int i=0; i<ar.nblabel; i++)
-		    {
-		      ar.result[i] = ar.result[i]/score_sum;
-		      if(ar.visual_result[i].size()>=50)
-			ar.visual_result[i].erase(ar.visual_result[i].begin());
-		      ar.visual_result[i].push_back(ar.result[i]);
-		    }
-	
-		  IplImage* graph=drawFloatGraph(&ar.visual_result[0][0], ar.visual_result[0].size(),NULL, 0, 1, 400,200);
-		  for(int i=1; i<ar.nblabel; i++)
-		    drawFloatGraph(&ar.visual_result[i][0], ar.visual_result[i].size() ,graph, 0, 1, 600,200);
-
-		  showImage(graph,10);
-		  ss<<"working: "<<ar.result[0]<<" shake hand: "<<ar.result[1]<<" drinking: "<<ar.result[2]<<endl;
-		  cout<<ss.str()<<endl;
-		  ar.counter=0;
-		  score_sum=0;
-		  action_result.data=ss.str();
-		  ar.action_result_pub.publish(action_result); //pub the result to euslisp to excute is enough
-		  std::fill(ar.result.begin(), ar.result.end(), 0);//clear the vector without change its init
-		  
+	  	      
 		  listener.clear();
 		  
-		}
-	   ar.counter++;*/
-	  
-
 	}
-      /*      vector<float> test;
-      for(float i=0.1; i<1; i=i+0.1)
-	test.push_back(i);
-      IplImage* graph=drawFloatGraph(&test[0], test.size(), NULL, 0, 1, 400,180) ;
-      showImage(graph,0);*/
-      transform_v.clear();
+
+        transform_v.clear();      
       rate.sleep();
 
     }
